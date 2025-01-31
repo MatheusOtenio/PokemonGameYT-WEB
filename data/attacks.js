46336954034080ded4775cdf4da2e7e2b3356df4
MatheusOtenio <146143568+MatheusOtenio@users.noreply.github.com>
2025-01-31 15:26:23 -0300

    Add files via upload

---
+++ data/attacks.js
@@ -0,0 +1,12 @@
+const attacks = {
+    Simples: {
+        name: 'Simples',
+        damage: 20,
+        type: 'Normal',
+    },
+    Fireball: {
+        name: 'Fireball',
+        damage: 50,
+        type: 'fire',
+    }
+}
